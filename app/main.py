ad29ef4890833a858cf794c0607b73c5146f8e10
michalgorul <michalgorul@gmail.com>
2022-11-15 22:47:32 +0100

    add cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
-from typing import Any, Dict
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from iq_air.router import router as iq_air_router
 
@@ -13,3 +12,17 @@ app = FastAPI(
 )
 
 app.include_router(iq_air_router)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
